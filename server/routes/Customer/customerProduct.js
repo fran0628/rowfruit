2c17c4842abd2dcacbd0942a1b98b2a66ea42a1c
ChecheHuang <80943702+ChecheHuang@users.noreply.github.com>
2021-07-14 00:51:43 +0800

    api

--- server/routes/Customer/customerProduct.js
+++ server/routes/Customer/customerProduct.js
@@ -3,9 +3,27 @@ const router = express.Router();
 const connection = require("../../utilities/db");
 
 router.get("/",async(req, res)=>{
-    let queryResult =await connection.queryAsync("SELECT * FROM customize_label");
-    console.log(queryResult)
-    res.send(queryResult)
+    let fruit = await connection.queryAsync("SELECT * FROM customize_label WHERE valid=0")
+    let storage =await connection.queryAsync("SELECT storage.id,fruittype,fruitname,price,images,	quantity,fram_name FROM storage INNER JOIN farmer_member ON storage.farmer_list_id=farmer_member.id ORDER BY storage.id");
+   
+    let item = [];
+  for(let n=0;n<fruit.length;n++){
+    for(let i=0;i<storage.length;i++){
+      if(storage[i].fruittype == fruit[n].id){
+        item.push(storage[i])
+        Object.assign(fruit[n], {"items": item});
+      }
+    }
+    item=[];
+  }
+    res.json(fruit);
+  });
+
+  router.get("/:item",async(req, res)=>{
+    let product = await connection.queryAsync("SELECT * FROM customize_label WHERE id=?",req.params.item);
+   
+    // console.log(product)
+    res.json(product)
   });
 
 module.exports = router ;
