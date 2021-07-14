73a7c3badc620dc8a1ae72248d9ba856f8dcdbb7
ChecheHuang <80943702+ChecheHuang@users.noreply.github.com>
2021-07-14 08:14:32 +0800

    odify

--- server/routes/Customer/customerProduct.js
+++ server/routes/Customer/customerProduct.js
@@ -2,28 +2,28 @@ const express = require("express");
 const router = express.Router();
 const connection = require("../../utilities/db");
 
-router.get("/",async(req, res)=>{
-    let fruit = await connection.queryAsync("SELECT * FROM customize_label WHERE valid=0")
-    let storage =await connection.queryAsync("SELECT storage.id,fruittype,fruitname,price,images,	quantity,fram_name FROM storage INNER JOIN farmer_member ON storage.farmer_list_id=farmer_member.id ORDER BY storage.id");
-   
-    let item = [];
-  for(let n=0;n<fruit.length;n++){
-    for(let i=0;i<storage.length;i++){
-      if(storage[i].fruittype == fruit[n].id){
-        item.push(storage[i])
-        Object.assign(fruit[n], {"items": item});
+router.get("/", async (req, res) => {
+  let fruits = await connection.queryAsync("SELECT * FROM customize_label WHERE valid=0")
+  //storage and farmer_member joinTable select
+  let storages = await connection.queryAsync("SELECT storage.id,fruittype,fruitname,price,images,	quantity,fram_name FROM storage INNER JOIN farmer_member ON storage.farmer_list_id=farmer_member.id ORDER BY storage.id");
+  fruits.forEach((fruit)=> {
+    let item = []
+    storages.forEach( (storage)=> {
+      if (storage.fruittype == fruit.id) {
+        item.push(storage)
+        Object.assign(fruit, { "items": item })
       }
-    }
+    })
     item=[];
-  }
-    res.json(fruit);
-  });
+  })
+    res.json(fruits);
+});
 
-  router.get("/:item",async(req, res)=>{
-    let product = await connection.queryAsync("SELECT * FROM customize_label WHERE id=?",req.params.item);
-   
-    // console.log(product)
-    res.json(product)
-  });
+router.get("/:item", async (req, res) => {
+  let product = await connection.queryAsync("SELECT * FROM customize_label WHERE id=?", req.params.item);
 
-module.exports = router ;
+  // console.log(product)
+  res.json(product)
+});
+
+module.exports = router;
