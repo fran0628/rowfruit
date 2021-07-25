70c23da34a5142003da8c3f865ec71171abfcb43
frank <isfran0628@gmail.com>
2021-07-25 14:24:01 +0800

    modify client/home.js & add public/fruitIcon

--- client-react/src/pages/Home.js
+++ client-react/src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import IndexFarmer from '../component/IndexFarmer/IndexFarmer'
-
+import IndexCustomAni from '../component/indexCustomAni/IndexCustomAni'
 
 function Home(){
     return (
 			<>
 				<h1>Home</h1>
+				<IndexCustomAni />
 				<IndexFarmer />
+				
 			</>
 		);
 }
