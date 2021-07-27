b4c63daf411a7d36c675e16bd624524f13c73759
frank <isfran0628@gmail.com>
2021-07-27 22:55:24 +0800

    add pages/cart , npm gsap , modify ToNav
    add pages/cart/productItem/summary/buyer

---
+++ client-react/src/pages/Cart/components/summary.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function summary() {
+  return (
+    <>
+      <table class="table table-success container">
+        <thead>
+          <h3 class="text-center"> 付款摘要</h3>
+          <tr>
+            <th scope="col">商品圖片</th>
+            <th scope="col">商品數量</th>
+            <th scope="col">商品總價</th>
+          </tr>
+          <tr>
+            <td>x</td>
+            <td>x</td>
+            <td>x</td>
+          </tr>
+        </thead>
+      </table>
+    </>
+  );
+}
+
+export default summary;
