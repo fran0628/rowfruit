4cccce87be84606f5c180437a8e4cd510eae32de
miaoyiban <83852328+miaoyiban@users.noreply.github.com>
2021-07-30 01:23:43 +0800

    indexFarmer

--- client-react/src/component/IndexFarmer/CardItem/CardItem.js
+++ client-react/src/component/IndexFarmer/CardItem/CardItem.js
@@ -2,6 +2,37 @@ import React from 'react'
 import './carditem.scss'
 
 
+const category = [
+	"apple",
+	"banana",
+	"guava",
+	"avocado",
+	"pineapple",
+	"tangerine",
+	"kiwi",
+	"papaya",
+	"watermelon",
+	"mango",
+];
+
+const categoryText = [
+	"蘋果",
+	"香蕉",
+	"芭樂",
+	"酪梨",
+	"鳳梨",
+	"柑橘",
+	"奇異果",
+	"木瓜",
+	"西瓜",
+	"芒果",
+];
+const convertCategoryToText = (v) => {
+	const index = category.indexOf(v);
+
+	return categoryText[index];
+};
+
 
 function CardItem(props) {
 	const {farmer}=props
@@ -11,16 +42,26 @@ function CardItem(props) {
 			<>
 				<div className="card border-0 rounded-0 farm-card me-5 fr-card">
 					<div className="if-rating position-absolute">
-						<img src={PF + farmer.rating +".png"} className="object-fit" alt="" />
+						<img
+							src={PF + farmer.rating + ".png"}
+							className="object-fit"
+							alt=""
+						/>
 					</div>
 					<div className="fr-top">
-						<img src={PF + farmer.avatar} className="card-img-top object-fit" alt="..." />
+						<img
+							src={PF + farmer.avatar}
+							className="card-img-top object-fit"
+							alt="..."
+						/>
 					</div>
 
 					<div className="card-body">
 						<h5 className="card-title">{farmer.fram_name}</h5>
 						<h6 className="card-subtitle mb-2 text-muted">
-							種植水果：{farmer.fruit[0]}、{farmer.fruit[1]}、{farmer.fruit[2]}
+							種植水果：{convertCategoryToText(farmer.fruit[0])}、
+							{convertCategoryToText(farmer.fruit[1])}、
+							{convertCategoryToText(farmer.fruit[2])}
 						</h6>
 
 						<p className="card-text">{farmer.content}</p>
