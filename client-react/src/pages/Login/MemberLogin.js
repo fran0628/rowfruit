c0c9d3da8ab075884ab60fa24988d401b741f9d2
Steven <alucky2108@gmail.com>
2021-07-30 15:54:17 +0800

    update member login

--- client-react/src/pages/Login/MemberLogin.js
+++ client-react/src/pages/Login/MemberLogin.js
@@ -1,5 +1,5 @@
-import axios from "axios";
-import React, { useState } from "react";
+// import axios from "axios";
+import React, { useState,useEffect } from "react";
 import { withRouter } from "react-router-dom";
 import "./MemberLogin.scss";
 
@@ -21,63 +21,167 @@ const banner = {
   backgroundSize: "cover",
 };
 
-  function MemberLogin(props) {
-  const {setAuth}=props
+function MemberLogin(props) {
+  const { setAuth } = props;
   const [account, setAccount] = useState("");
 
   function accountChange(e) {
-    setAccount(e.target.value)
+    setAccount(e.target.value);
   }
+
   const [password, setPassword] = useState("");
 
   function passwordChange(e) {
-    setPassword(e.target.value)
+    setPassword(e.target.value);
   }
-  const [login, setLogin] = useState("");
+  // const [login, setLogin] = useState([]);
 
-  function loginChange(e) {
-    setLogin(e.target.value)
+  // function loginChange() {
+  //   account:"111111",
+  //   password:"111111"
+  // }
+
+
+  const [isLoading, setIsLoading] = useState(false)
+  // 自動1秒後關閉指示的spinner
+  useEffect(() => {
+    if (isLoading) {
+      setTimeout(() => setIsLoading(false), 1000)
+    }
+  }, [isLoading])
+  // 狀態變為物件，處理多個欄位
+  const [fields, setFields] = useState({
+    username: '',
+    password: '',
+  })
+
+  // 每個欄位的錯誤訊息
+  const [fieldErrors, setFieldErrors] = useState({
+    username: '',
+    password: '',
+  })
+
+  const handleFieldChange = (e) => {
+    // 更新輸入欄位的變動
+    // 用新輸入的屬性值和原物件作合併
+    const updatedFields = {
+      ...fields,
+      [e.target.name]: e.target.value,
+    }
+
+    setFields(updatedFields)
   }
+
+  
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const data = new FormData(e.target);
+    const user = new FormData(e.target);
 
-    console.log(data.get("account"));
-    console.log(data.get("password"));
+    console.log(user.get("account"));
+    console.log(user.get("password"));
+
+    // 整個表單有任何變動(ex.某個欄位有輸入)
+  // 認為使用者正在改有錯誤的欄位
+  // 清除某個欄位的錯誤訊息
+ 
 
     // ex. 送到伺服器
   };
-  function changeLogin(){
-    setAuth("Steven")
+  function changeLogin() {
+    setAuth("Steven");
   }
 
-  function loginApi (){
-    let data = {"account":666666,"password":666666};
+  function loginApi() {
+    // let data = { account: 123456, password: 123456 };
 
-    // let data = {
-    //   account=this.account,
-    //   password=this.password
-    // };
-    axios.post(`http://localhost:5000/api/login`,data)
-  
-    .then(res=>{
-        localStorage.setItem('token',res.data.data.token);
-        // token解析
-        const token =  res.data.data.token.split(' ')[1];
-        
-        let payload =JSON.parse(atob(token.split('.')[1]));
-  
-        console.log('res=>',payload);
-        console.log(res.data.data.name);            
+    // axios
+    //   .post(`http://localhost:5000/api/login`, data)
+
+    //   .then((res) => {
+    //     localStorage.setItem("token", res.data.data.token);
+    //     // token解析
+    //     const token = res.data.data.token.split(" ")[1];
+
+    //     let payload = JSON.parse(atob(token.split(".")[1]));
+
+    //     console.log("res=>", payload);
+    //     console.log(res.data.data.name);
+    //   });
+
+    let url = "http://localhost:5000/api/login";
+    let data = { account: '123456', password: '123456' };
+
+    fetch(url, {
+      method: "POST", // or 'PUT'
+      body: JSON.stringify(data), // data can be `string` or {object}!
+      headers: new Headers({
+        "Content-Type": "application/json",
+
+      }),
     })
-  
+      .then( res => {
+        return res.json();
+      }).then(result => {
+         localStorage.setItem("token", result.data.token);
+        // token解析
+        const token = result.data.token.split(" ")[1];
+
+        let payload = JSON.parse(atob(token.split(".")[1]));
+
+        // console.log("res :",res)
+
+        console.log("res=>", payload);
+        console.log(result.data.name);
+      });
+
+      // .catch((error) => console.error("Error:", error))
+      // .then((response) => console.log("Success:", response));
   }
 
-  return (
-    <div>
-      <button onClick={changeLogin}>我會改變login</button>
+  const handleFormChange = (e) => {
+    console.log('更新欄位: ', e.target.value)
+
+    // 該欄位的錯誤訊息清空
+    const updatedFieldErrors = {
+      ...fieldErrors,
+      [e.target.name]: '',
+    }
+
+    setFieldErrors(updatedFieldErrors)
+  }
+
+  // 表單有不合法的檢查出現時
+  const handleFormInvalid = (e) => {
+    // 擋住錯誤訊息預設呈現方式(跳出的訊息泡泡)
+    // e.preventDefault()
+
+    const updatedFieldErrors = {
+      ...fieldErrors,
+      [e.target.name]: e.target.validationMessage,
+    }
+
+    setFieldErrors(updatedFieldErrors)
+  }
+  
+  const loading = (
+    <>
+      <div className="d-flex justify-content-center">
+        <div className="spinner-border" role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+      </div>
+    </>
+  )
+
+  const display = (
+    // <button onClick={changeLogin}>我會改變login</button>
+    <form
+      onSubmit={handleSubmit}
+      onChange={handleFormChange}
+      onInvalid={handleFormInvalid}
+    >
       <div className="MemberBanner" style={banner}>
         {/* Login  Form */}
         <div className="container">
@@ -86,7 +190,7 @@ const banner = {
               <div className="card shadow">
                 <div className="MemberCardLogo text-center border-bottom">
                   {/* ROWFRUIT*/}
-                  <img className="logo p-3" src="MemberPhoto/logo.svg" alt="" />
+                  <img className="87 p-3" src="MemberPhoto/logo.svg" alt="" />
                 </div>
                 <div className="MemberBody">
                   <h3 className="NormalMemberLogin d-flex justify-content-center mb-3">
@@ -98,12 +202,15 @@ const banner = {
                         帳號
                       </label>
                       <input
-                        className="form-control"
+                        className={`form-control ${
+                          fieldErrors.account !== "" ? "is-invalid" : ""
+                        }`}
                         type="text"
                         name="account"
+                        setState={handleFieldChange}
+                        value={fields.account}
                         placeholder="Account"
-                        value={account}
-                        onChange={accountChange}
+                        onChange={handleFieldChange}
                         required
                       />
                     </div>
@@ -112,12 +219,18 @@ const banner = {
                         密碼
                       </label>
                       <input
-                        className="form-control"
+                        className={`form-control ${
+                          fieldErrors.password !== "" ? "is-invalid" : ""
+                        }`}
                         type="password"
                         name="password"
+                        state={fields.password}
+                        setState={handleFieldChange}
+                        error={fieldErrors.password}
                         placeholder="Password"
-                        value={password}
-                        onChange={passwordChange}
+                        onChange={handleFieldChange}
+                        minLength="6"
+                        maxLength="8"
                         required
                       />
                     </div>
@@ -131,15 +244,22 @@ const banner = {
                     </div>
                     <div className="d-none d-sm-flex justify-content-center">
                       <button type="submit" className="btn normal-btn"
-                      //  onChange={loginChange}
-                       onClick={loginApi}>
+                      onClick={loginApi}>
                         登入
                       </button>
                     </div>
+                    {/* <button
+                      type="button"
+                      onClick={() => {
+                        checkLogin();
+                      }}
+                      className="btn btn-primary"
+                    >
+                      檢查登入狀態
+                    </button> */}
                     <div className=" d-sm-none my-2">
                       <button type="submit" className="btn rwd-btn"
-                      //  onChange={loginChange}
-                       onClick={loginApi}>
+                      onClick={loginApi}>
                         登入
                       </button>
                     </div>
@@ -173,7 +293,14 @@ const banner = {
           </div>
         </div>
       </div>
-    </div>
+    </form>
   );
+   return (
+    <>
+      {/* <h1>會員登入</h1> */}
+      <hr />
+      {isLoading ? loading : display}
+    </>
+  )
 }
 export default withRouter(MemberLogin);
