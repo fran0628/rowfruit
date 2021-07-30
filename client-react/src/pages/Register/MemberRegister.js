fac3c73c2642fad2d6ccf90a4afeb465db0abfd0
Steven <alucky2108@gmail.com>
2021-07-30 21:21:03 +0800

    update  register

--- client-react/src/pages/Register/MemberRegister.js
+++ client-react/src/pages/Register/MemberRegister.js
@@ -1,6 +1,6 @@
-import React, { useState,useEffect } from "react";
+import React, { useState } from "react";
 import "./MemberRegister.scss";
-import axios from 'axios'
+// import axios from 'axios'
 
 
 
