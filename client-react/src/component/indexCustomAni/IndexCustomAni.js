6d6830d796e269d97f7cca0c6fff56ce2e7253c1
frank <isfran0628@gmail.com>
2021-08-01 18:37:28 +0800

    modify indexCustomAni

--- client-react/src/component/indexCustomAni/IndexCustomAni.js
+++ client-react/src/component/indexCustomAni/IndexCustomAni.js
@@ -1,10 +1,14 @@
 import React from "react";
+import FruitIconItem from "./fruitIcon/FruitIconItem.js";
 import "./indexCustomAni.scss";
 
-function IndexCustomAni() {
+function IndexCustomAni(props) {
   // 客制化水果盒按鈕連結
   const urlid = String();
   const urlcustombtn = "/customized/" + urlid;
+  const FruitIconItemList1 = ["apple.png"];
+  const FruitIconItemList2 = ["avocado.png"];
+  const FruitIconItemList3 = ["banana.png"];
   return (
     <>
       <div className="container">
@@ -20,13 +24,25 @@ function IndexCustomAni() {
         </div>
         <div className="container-fluid section">
           <h1 className="title1">客製化水果</h1>
-          <div class="at-item1">a</div>
-          <div class="at-item2">b</div>
-          <div class="at-item3">c</div>
+          <div class="at-item1">
+            {FruitIconItemList1.map((Img) => {
+              return <FruitIconItem Img={Img} />;
+            })}
+          </div>
+          <div class="at-item2">
+            {FruitIconItemList2.map((Img) => {
+              return <FruitIconItem Img={Img} />;
+            })}
+          </div>
+          <div class="at-item3">
+            {FruitIconItemList3.map((Img) => {
+              return <FruitIconItem Img={Img} />;
+            })}
+          </div>
           <h1 className="title2">親手選擇當季最新鮮的水果</h1>
           <h4 className="title3">吃的開心,吃得健康,滿足您全家人的喜好</h4>
           <a href={urlcustombtn}>
-          <button className="gotocustombtn">前往購買!</button>
+            <button className="gotocustombtn">前往購買!</button>
           </a>
           {/* <h3 className="title4">打造專屬於你的水果盒 讓您與眾不同</h3> */}
         </div>
