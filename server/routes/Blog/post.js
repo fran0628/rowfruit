646ad82e23cd5b62954af94132c19787e99375cf
miaoyiban <83852328+miaoyiban@users.noreply.github.com>
2021-08-02 19:46:49 +0800

    cat debug

--- server/routes/Blog/post.js
+++ server/routes/Blog/post.js
@@ -5,35 +5,29 @@ const connection = require("../../utilities/db");
 // get的部分
 // 全部文章
 router.get("/", async (req, res) => {
-	const articles = await connection.queryAsync(
-		"SELECT * FROM article;"
-		
-	);
-	
+	const articles = await connection.queryAsync("SELECT * FROM article;");
 
 	res.json(articles);
 });
+
+// 各類別
+router.get("/cat/:cat", async (req, res) => {
+	const aticleCat = await connection.queryAsync(
+		"SELECT * FROM article WHERE category = ?;",
+		req.params.cat
+	);
+
+	res.json(aticleCat);
+});
+
 // 單篇文章
 router.get("/:id", async (req, res) => {
 	const aticle = await connection.queryAsync(
 		"SELECT * FROM article WHERE id = ?;",
 		req.params.id
 	);
-	
 
 	res.json(aticle);
 });
-// 各類別
-router.get("/:cat", async (req, res) => {
-	const aticleCat = await connection.queryAsync(
-		"SELECT * FROM article WHERE category = ?;",
-		req.params.cat
-	);
-	
-
-	res.json(aticleCat);
-});
-
-
 
 module.exports = router;
