af8294c6804db359a5a8465b8c21d9c45f5e2f56
ChecheHuang <80943702+ChecheHuang@users.noreply.github.com>
2021-08-05 18:02:15 +0800

    modify

--- client-react/src/pages/Blog/BlogPage/BlogPage.js
+++ client-react/src/pages/Blog/BlogPage/BlogPage.js
@@ -1,57 +1,65 @@
 import React, { useState, useEffect } from "react";
-import HotPost from './HotPost/HotPost'
-import './blogpage.css'
+import HotPost from "./HotPost/HotPost";
+import "./blogpage.css";
 import axios from "axios";
 import PostBlock from "./PostBlock/PostBlock";
 import PostAside from "./PostAside/PostAside";
-import { useLocation,Link } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 import MultiLevelBreadcrumb from "../../../component/BreadCrumb/MultiLevelBreadcrumb";
 
 const category = ["Fruit", "History", "Knowledge", "Live", "Other"];
 
 const categoryText = ["水果", "歷史", "知識", "生活", "其他"];
 const convertCategoryToText = (v) => {
-	const index = category.indexOf(v);
+  const index = category.indexOf(v);
 
-	return categoryText[index];
+  return categoryText[index];
 };
 
 function deleteHtmlTag(str) {
-	str = str.replace(/<[^>]+>|&[^>]+;/g, "").trim(); 
-	return str;
+  str = str.replace(/<[^>]+>|&[^>]+;/g, "").trim();
+  return str;
 }
 
+function BlogPage() {
+  const [posts, setPosts] = useState([]);
+  const [hotArticle, setHotArticle] = useState([]);
+  const [dataLoading, setDataLoading] = useState(false);
 
+  const { search } = useLocation();
+  console.log(search.slice(1, search.length));
 
- function BlogPage() {
-    const [posts, setPosts] = useState([]);
-    const [hotArticle,setHotArticle]=useState([])
+  useEffect(() => {
+    setTimeout(() => setDataLoading(false), 1500);
+  }, []);
 
-	const { search } = useLocation();
-	console.log(search.slice(1,search.length));
+  // const serverUrl = "http://localhost:5000/api"
+  useEffect(() => {
+    const fetchPosts = async () => {
+      // const cat=
+      const res = await axios.get(
+        "/post/" + (search ? `cat/${search.slice(1, search.length)}` : "")
+      );
+      console.log(res);
 
+      console.log(res.data);
+      setPosts(res.data);
+      setHotArticle(res.data.slice(0, 5));
+	  setDataLoading(true)
+    };
 
-		// const serverUrl = "http://localhost:5000/api"
-		useEffect(() => {
-			const fetchPosts = async () => {
-				// const cat= 
-				const res = await axios.get("/post/" + (search ? `cat/${search.slice(1, search.length)}`: ""));
-				console.log(res);
+    fetchPosts();
+  }, [search]);
 
-				console.log(res.data);
-				setPosts(res.data);
-				setHotArticle(res.data.slice(0, 5));
-			};
+  // console.log(posts[0])
 
-			fetchPosts();
-		}, [search]);
-			
-			// console.log(posts[0])
-			
-		
-
-    return (
-			<>
+  return <>
+	  {dataLoading? 
+	  <div className="d-flex justify-content-center align-items-center vh-100">
+            <div className="spinner-border" role="status">
+              <span className="sr-only">Loading...</span>
+            </div>
+          </div>: <>
 				<MultiLevelBreadcrumb />
 				<div className="container mt-2">
 					<div className="section-hero row">
@@ -119,7 +127,7 @@ function deleteHtmlTag(str) {
 						<PostAside hotArticle={hotArticle} />
 					</div>
 				</div>
-			</>
-		);
+			</>}
+  </>;
 }
 export default BlogPage;
--- client-react/src/pages/Blog/FarmList/FarmList.js
+++ client-react/src/pages/Blog/FarmList/FarmList.js
@@ -1,35 +1,51 @@
-import React,{useState,useEffect} from 'react'
-import Farmer from './Farmer/Farmer';
-import axios from 'axios'
-import MultiLevelBreadcrumb from '../../../component/BreadCrumb/MultiLevelBreadcrumb';
+import React, { useState, useEffect } from "react";
+import Farmer from "./Farmer/Farmer";
+import axios from "axios";
+import MultiLevelBreadcrumb from "../../../component/BreadCrumb/MultiLevelBreadcrumb";
 
- function FarmList() {
-	const [farmers, setFarmers] = useState([]);
-    // const serverUrl = "http://localhost:5000/api"
-    useEffect(() => {
-			const fetchFarmers = async () => {
-				const res = await axios.get("/farmer");
-				console.log(res);
-				setFarmers(res.data);
-                // console.log(res.data)
-			};
+function FarmList() {
+  const [farmers, setFarmers] = useState([]);
+  const [dataLoading, setDataLoading] = useState(false);
+  useEffect(() => {
+    setTimeout(() => setDataLoading(false), 1500);
+  }, []);
 
-			fetchFarmers();
-            
-		}, []);
+  // const serverUrl = "http://localhost:5000/api"
+  useEffect(() => {
+    const fetchFarmers = async () => {
+      const res = await axios.get("/farmer");
+      console.log(res);
+      setFarmers(res.data);
+      // console.log(res.data)
+    };
 
-    return (
-			<>
-				<MultiLevelBreadcrumb />
-				<div className="container">
-					<h1 className="text-center mt-0 mb-0">小農部落</h1>
-					<div className="row g-5 mt-0" id="cards">
-						{farmers.map((f, i) => {
-							return <Farmer farmer={f} key={i} />;
-						})}
-					</div>
-				</div>
-			</>
-		);
+    fetchFarmers();
+    setDataLoading(true);
+    setTimeout(() => setDataLoading(false), 500);
+  }, []);
+
+  return (
+    <>
+      {dataLoading ? (
+        <div className="d-flex justify-content-center align-items-center vh-100">
+          <div className="spinner-border" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
+      ) : (
+        <>
+          <MultiLevelBreadcrumb />
+          <div className="container">
+            <h1 className="text-center mt-0 mb-0">小農部落</h1>
+            <div className="row g-5 mt-0" id="cards">
+              {farmers.map((f, i) => {
+                return <Farmer farmer={f} key={i} />;
+              })}
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
 }
-export default FarmList
+export default FarmList;
