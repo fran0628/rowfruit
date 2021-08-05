fc982fdf2a8e2f433e7681e2d9da8ac2237a8334
frank <isfran0628@gmail.com>
2021-08-05 17:59:37 +0800

    modify

--- client-react/src/component/indexCustomAni/fruitIcon/FruitIconItem.js
+++ client-react/src/component/indexCustomAni/fruitIcon/FruitIconItem.js
@@ -3,8 +3,8 @@ import React from "react";
 
 function FruitIconItem(props) {
   const { Img } = props;
-  const fruitIcon = "http://localhost:3000/indexCustomAniIcon/" + Img;
-  console.log(fruitIcon);
+  const fruitIcon = "http://localhost:3000/indexCustomAniIcon/indexCustom/" + Img;
+  // console.log(fruitIcon);
 
   //   function clickMe() {
   //     Swal.fire({
@@ -16,7 +16,7 @@ function FruitIconItem(props) {
   return (
     <>
       <div className="fruit_ball" id="clickMe">
-        <img src={fruitIcon} alt="avocado" />
+        <img src={fruitIcon} alt="rowfruit" />
       </div>
     </>
   );
