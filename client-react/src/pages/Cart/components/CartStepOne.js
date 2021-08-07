d2bff4ff87845590a9761668e0db6b1712c06523
ChecheHuang <80943702+ChecheHuang@users.noreply.github.com>
2021-08-07 17:28:53 +0800

    modify

--- client-react/src/pages/Cart/components/CartStepOne.js
+++ client-react/src/pages/Cart/components/CartStepOne.js
@@ -6,11 +6,12 @@ function CartStepOne(props) {
   useEffect(()=>{
     window.scrollTo(0, 0)
   },[])
+  console.log(myCart.length)
   return (
     <>
       <ProductTable myCart={myCart} setMyCart={setMyCart} setCartUpdate={setCartUpdate} />
       <div className="d-flex justify-content-center pt-3">
-        {myCart===[] ?  <>
+        {myCart.length !==0 ?  <>
         <span onClick={()=>{props.history.push("./Main")}} className="btn normal-btn mx-4 my-3">繼續購物</span>
         <span onClick={()=>{setStep(2)}} className="btn normal-btn mx-4 my-3">下一步</span>
         </> : <h1>請先購買水果</h1> }
