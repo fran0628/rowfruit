973fc2de39b3992d7c3582c5740ee20e7bed34f8
tung <abbe00021@gmail.com>
2021-08-09 01:18:07 +0800

    add subcartsql

--- server/routes/Suborder/Suborderlist.js
+++ server/routes/Suborder/Suborderlist.js
@@ -12,11 +12,11 @@ router.get("/", async (req, res) => {
 router.post("/", async (req, res) => {
   try {
     //拿到前端東西存到變數裡面
-    const {user_id, product_id, subscribe_way, receiver, phone, address} = req.body;
+    const {memberId, product_id, subscribe_way, receiver, phone, address} = req.body;
 
     await connection.queryAsync(
-      "INSERT INTO subscribe (user_id, product_id, subscribe_way, receiver, phone, address, start_time, end_time) VALUES (?, ?, ?, ?, ?, ?, CURRENT_TIME(),CURRENT_TIME())",
-      [user_id, product_id, subscribe_way, receiver, phone, address]
+      "INSERT INTO subscribe (user_id, product_id, subscribe_way, receiver, phone, address, start_time, end_time, status, valid) VALUES (?, ?, ?, ?, ?, ?, CURRENT_TIME(),CURRENT_TIME(), 1, 1)",
+      [memberId, product_id, subscribe_way, receiver, phone, address]
     );
 
     res.status(200).json({ success: true});
