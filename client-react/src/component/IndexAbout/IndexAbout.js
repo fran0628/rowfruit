41509a7674a1469173b447e7b5a93dafa948d6dc
JimH <iiijimhsu810926@gmail.com>
2021-08-10 01:54:15 +0800

    modify subscribeorderlist/indexabout

--- client-react/src/component/IndexAbout/IndexAbout.js
+++ client-react/src/component/IndexAbout/IndexAbout.js
@@ -11,40 +11,19 @@ export default function IndexAbout() {
     <div className="container-fluid section">
           <h1 >了解我們</h1>
           <div class="at-item1"></div>
-<CardGroup> 
-  <Card>
-    <Card.Img variant="top" src="/About/farmer.png" width="320" height="160" />
+<Card>
+    <Card.Img variant="top" src="/About/indexabout.jpg" width="50" height="500"/>
     <Card.Body>
-      <Card.Title>關於我們</Card.Title>
       <Card.Text>
-      品牌故事
+      我們網站的服務<br/>主要是販售搭配好的主題水果盒 可以提供單買或是訂閱制 <br/>訂閱制服務主要是定時每個星期配送水果盒到客人家中，讓客人每個禮拜不愁為了買水果<br/>還要出門<br/>那除了上述的服務外 我們也提供客制化水果盒<br/>這個服務主要可以讓客人指定購買喜歡的水果搭配
       </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-    <div className="d-none d-sm-flex justify-content-center  my-4">
+      <div className="d-none d-sm-flex justify-content-center  my-4">
     <a href={urlanoutbtn}>
           <button type="submit" className="btn normal-btn">
-            點此進入
+            了解更多
           </button></a></div>
-    </Card.Footer>
-  </Card>
- 
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>聯絡我們</Card.Title>
-      <Card.Text>
-      客服服務
-      </Card.Text>
     </Card.Body>
-    <Card.Footer>
-    <div className="d-none d-sm-flex justify-content-center  my-4">
-          <button type="submit" className="btn normal-btn">
-            點此進入
-          </button></div>
-    </Card.Footer>
   </Card>
-</CardGroup>
 </div>
 </div></>
 		);
