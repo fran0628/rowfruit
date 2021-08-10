4f1ce08a5f2ae3ef07d8274fbf8b570f46e342f5
frank <isfran0628@gmail.com>
2021-08-10 15:22:01 +0800

    modify

--- client-react/src/component/IndexAbout/IndexAbout.js
+++ client-react/src/component/IndexAbout/IndexAbout.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import './indexabout.scss'
 import { Card } from "react-bootstrap";
 import { CardGroup } from "react-bootstrap";
 export default function IndexAbout() {
@@ -11,8 +11,9 @@ export default function IndexAbout() {
     <div className="container-fluid section">
           <h1 >了解我們</h1>
           <div class="at-item1"></div>
-<Card>
-    <Card.Img variant="top" src="/About/indexabout.jpg" width="50" height="500"/>
+<Card className="border-0">
+    <div className="kr-contain mx-auto"><Card.Img variant="top" src="/About/indexabout.jpg" className="object-fit"/></div>
+    
     <Card.Body>
       <Card.Text>
       我們網站的服務<br/>主要是販售搭配好的主題水果盒 可以提供單買或是訂閱制 <br/>訂閱制服務主要是定時每個星期配送水果盒到客人家中，讓客人每個禮拜不愁為了買水果<br/>還要出門<br/>那除了上述的服務外 我們也提供客制化水果盒<br/>這個服務主要可以讓客人指定購買喜歡的水果搭配
